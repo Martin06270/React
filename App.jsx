3f9996cf040965373b76250f91330db11c9194d6
Martin06270 <martinng06@hotmail.com>
2025-03-13 11:30:23 +0100

    #

--- App.jsx
+++ App.jsx
@@ -2,13 +2,24 @@
 import { useState } from 'react';
 
 const App = () => {
-    useState(); //  c'est l'état locale d'un composant hook c'est une fonction qui execute une tache bien précise .
+    useState(); //  c'est l'état locale d'un composant hook c'est une fonction qui execute une tache bien précise ;
 
-    let count = 0;
+    let count = 0; // ;
 
     const handleIncrement = () => {
         count++;
         console.log(count);
 
     }
-}
+
+
+    return {
+       <>
+        <h1> Comprendre useState </h1>
+
+
+     </>
+
+
+    }
+};
